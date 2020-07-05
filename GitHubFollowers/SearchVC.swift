687b171f66e8493b3c9f08106e85cb84f215ab76
asapkent <jeffersalex94@gmail.com>
2020-07-05 11:23:12 -0400

    Creating TabBar and TabBarVC's.

---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Robert Jeffers on 7/5/20.
+//  Copyright © 2020 AsapInc. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemTeal
+    }
+    
+
+}
