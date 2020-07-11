bcf339441fdcf29ae4e1c79ff5702c9b71f371c5
asapkent <jeffersalex94@gmail.com>
2020-07-11 15:37:20 -0400

    More collection view set up.

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -11,26 +11,56 @@ import UIKit
 class FollowerListVC: UIViewController {
 
     var userName: String!
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-        NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
-            
-            switch result {
-            case .success(let followers):
-            print(followers)
-            case .failure(let error):
-                 self.presentGFAlertOnMainThread(title: "Test", message: error.rawValue, buttonTitle: "Ok")
-            }
-        }
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
- 
-
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        // the + 40 gives extra space for the label
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+    
+      return flowLayout
+    }
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func getFollowers() {
+        NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
+                   
+               switch result {
+               case .success(let followers):
+               print(followers)
+               case .failure(let error):
+                    self.presentGFAlertOnMainThread(title: "Test", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
 }
